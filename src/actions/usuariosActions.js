0a27f4682503eb760d9fa914b4fcf8719372424d
Gerardo Garavito <garavitogerardo@gmail.com>
2022-04-27 00:30:41 -0500

    Handle async process, add error and loading state

--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -1,12 +1,21 @@
 import axios from 'axios';
+import { TRAER_TODOS, CARGANDO, ERROR } from '../types/usuariosTypes';
 
 export const traerTodos = () => async (dispatch) => {
-  const response = await axios.get(
-    'https://jsonplaceholder.typicode.com/users'
-  );
+  dispatch({ type: CARGANDO });
 
-  dispatch({
-    type: 'traer_usuarios',
-    payload: response.data,
-  });
+  try {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+    dispatch({
+      type: TRAER_TODOS,
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ERROR,
+      payload: error.message,
+    });
+  }
 };
